a8a99b10d2bd6740f033498f81ce19ee6c352337
prasad-kumkar <pk111_@live.com>
2022-01-02 03:23:07 +0530

    deployment

--- contracts/Operator.sol
+++ contracts/Operator.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.7.0;
+pragma solidity >=0.4.22 <0.9.0;
 
 import "@chainlink/contracts/src/v0.7/Operator.sol";
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,13 +1,47 @@
 const Operator = artifacts.require("Operator");
+const TestToken = artifacts.require("TestToken");
 
 require("dotenv").config({path: "../.env"});
 
 module.exports = async function (deployer, network, accounts) {
+  let sender, token;
 
-  await deployer.deploy(Operator, process.env.SCORE_TOKEN, accounts[0]);
+  switch(network){
+    case "hm": {
+      token = process.env.SCORE_TOKEN_HM;
+      sender = process.env.ORACLE_NODE_HM;
+      break;
+    }
+    case "ht": {
+      token = process.env.SCORE_TOKEN_HT;
+      sender = process.env.ORACLE_NODE_HT;
+      break;
+    }
+    case "rinkeby": {
+      token = process.env.SCORE_TOKEN_RINKEBY;
+      sender = process.env.ORACLE_NODE_RINKEBY;
+      break;
+    }
+    case "am": {
+      token = process.env.SCORE_TOKEN_AM;
+      sender = process.env.ORACLE_NODE_AM;
+      break;
+    }
+    case "at": {
+      token = process.env.SCORE_TOKEN_AT;
+      sender = process.env.ORACLE_NODE_AT;
+      break;
+    }
+    default: {
+      await deployer.deploy(TestToken);
+      token = (await TestToken.deployed()).address;
+    }
+  }
+
+  await deployer.deploy(Operator, token, accounts[0]);
   let operatorInstance = await Operator.deployed();
 
-  await operatorInstance.setAuthorizedSenders([process.env.ORACLE_NODE]);
+  await operatorInstance.setAuthorizedSenders([sender]);
 
   await operatorInstance.transferOwnership(process.env.ADMIN_WALLET);
 };
